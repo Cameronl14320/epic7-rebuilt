c6323410aaef341a593c25aee7509f5bca8727f6
Cameron Li <41677875+Cameronl14320@users.noreply.github.com>
2021-01-26 13:31:19 +1300

    creating initial gear visual structure

--- pages/gear.tsx
+++ pages/gear.tsx
@@ -1,12 +1,40 @@
 import GearSelection from '../objects/GearSelection'
-import {SubStat, SubArray} from '../objects/SubStat'
 import GearSettings from '../objects/GearSettings'
+import {SubStat, SubArray} from '../objects/SubStat'
+import { Box } from 'rebass'
 
 const selection = new GearSelection();
-const settings = new GearSettings();
+const settings = new GearSettings(0, 0 ,0);
 
-export default function(props) {
+const gearStyle = {
+    wrapper: {
+        display: 'flex',
+        justifyContent: "center",
+    },
+    centerContainer: {
+
+    },
+    grid: {
+        display: 'grid'
+    },
+    compute: {
+        display: 'flex'
+    }
+}
+
+export default function gear(props) {
     return (
-        <></>
+        <Box id="gear-wrapper" sx={gearStyle.wrapper}>
+            <Box id="gear-center-container" sx={gearStyle.centerContainer}>
+                <Box id="gear-grid" sx={gearStyle.grid}>
+                    <Box id="gear-grid-row-1"></Box>    
+                    <Box id="gear-grid-row-2"></Box>    
+                    <Box id="gear-grid-row-3"></Box>    
+                </Box>
+                <Box id="gear-compute" sx={gearStyle.compute}>
+                    456
+                </Box>
+            </Box>
+        </Box>        
     )
 }
