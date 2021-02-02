ad2cfc8123609c6684c6e6cfd9e924447d91dc5b
Cameron Li <41677875+Cameronl14320@users.noreply.github.com>
2021-02-02 16:29:02 +1300

    new slider component to test

--- components/gear/enhance/EnhanceSelect.tsx
+++ components/gear/enhance/EnhanceSelect.tsx
@@ -1,8 +1,8 @@
 import { Box } from 'rebass'
 import settings from '../../../data/settings'
 import { Button, Color } from '../../../styles/theme'
-import Slider, { Range } from 'rc-slider';
-import 'rc-slider/assets/index.css';
+import { Slider, Rail, Handles, Tracks, Ticks } from 'react-compound-slider';
+import { Handle, Track, Tick } from '../../shared/slider'; // example render components
 
 const maxEnhance = settings.maxEnhance;
 
@@ -22,17 +22,20 @@ const enhanceStyle = {
     },
 
     slider: {
-        handleStyle: {
+        sliderStyle: {
+            margin: '5%',
+            position: 'relative',
+            width: '90%',
+            height: '50px',
         },
         railStyle: {
-
-        },
-        dotStyle: {
-            borderColor: Color.theme.primary,
-        },
-        activeDotStyle: {
-
-        },
+            position: 'absolute',
+            width: '100%',
+            height: 14,
+            borderRadius: 7,
+            cursor: 'pointer',
+            backgroundColor: 'rgb(155,155,155)'
+        }
     },
 }
 
@@ -41,23 +44,70 @@ export interface EnhanceSelectProps {
     onSelect: (enhance: number) => void
 }
 
+const formatTicks = (d: number) => {
+    return `${d}`;
+  };
+
+
 export default function EnhanceSelect(props : EnhanceSelectProps) {
     const { selectedEnhance, onSelect } = props;
 
     var enhanceSelection = []
-    
+
 
     return (
-            <Slider
-                min={0} max={5} step={1} dots={true} 
-                handleStyle={enhanceStyle.slider.handleStyle}
-                railStyle={enhanceStyle.slider.railStyle}
-                dotStyle={enhanceStyle.slider.dotStyle} 
-                activeDotStyle={enhanceStyle.slider.activeDotStyle}
-                defaultValue={selectedEnhance}
-                ariaLabelForHandle={selectedEnhance.toString()}
-                onChange={(value) => {onSelect(value)}}    
-            >
-            </Slider>
+        <Slider
+            rootStyle={enhanceStyle.slider.sliderStyle}
+            domain={[0, 15]}
+            step={3}
+            values={[0, 3, 6, 9, 12, 15]}
+            mode={1}
+            onChange={() => onSelect}
+        >
+            <Rail>
+                {({ getRailProps }) => (
+                    <Box sx={enhanceStyle.slider.railStyle} {...getRailProps()} />
+                )}
+
+            </Rail>
+            <Handles>
+
+                {({ handles, getHandleProps }) => (
+                    <Handle
+                    handle={handles[0]}
+                    domain={[0, 15]}
+                    getHandleProps={getHandleProps}
+                    />
+                )}
+            </Handles>
+            <Tracks>
+                {({ tracks, getTrackProps }) => (
+                <Box className="slider-tracks">
+                    {tracks.map(({ id, source, target }) => (
+                    <Track
+                        key={id}
+                        source={source}
+                        target={target}
+                        getTrackProps={getTrackProps}
+                    />
+                    ))}
+                </Box>
+                )}
+            </Tracks>
+            <Ticks values={[0, 3, 6, 9, 12, 15]}>
+                {({ ticks }) => (
+                <Box className="slider-ticks">
+                    {ticks.map(tick => (
+                    <Tick
+                        key={tick.id}
+                        format={formatTicks}
+                        tick={tick}
+                        count={ticks.length}
+                    />
+                    ))}
+                </Box>
+                )}
+            </Ticks>
+        </Slider>
     )
 }
--- styles/theme.tsx
+++ styles/theme.tsx
@@ -1,20 +1,19 @@
-export const Button = {
-    padding: '5px',
-    mx: '5px',
-    borderRadius: '5px',
-
-
-    ":checked": {
-        boxShadow: "0 0 0 3px gold",
-        borderRadius: ['5px', '4px', '3px']
-    }
-}
-
 export const Color = {
     theme: {
         primary: 'black',
         secondary: 'white',
         primarybeta: 'darkgray',
-        secondarybeta: 'silver'
+        secondarybeta: 'silver',
+        selected: 'gold',
+    }
+}
+
+export const Button = {
+    padding: '5px',
+    mx: '5px',
+    borderRadius: '5px',
+    ":checked": {
+        boxShadow: "0 0 0 3px " + Color.theme.selected,
+        borderRadius: ['5px', '4px', '3px']
     }
 }
